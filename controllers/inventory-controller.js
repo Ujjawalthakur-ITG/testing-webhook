ca0986e59ceee229c8c8b9efbd2ddb29268f4089
ujjwal <ujjwalthakur@itgeeks.com>
2025-09-29 18:18:41 +0530

    changes done

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -4,7 +4,11 @@ const axios = require("axios");
 // Express handler for webhook POST request
 const webhookCreate = async (req, res) => {
   try {
-    console.log(req.body, "my payload");
+     const order = req.body;
+        orderData = JSON.stringify(order, null, 2);
+        conso.log(" New Order Rleeceived from Miva");
+        console.log(orderData);
+ 
     // You can do further processing here, e.g., call axios, etc.
 
     res.status(200).send("Webhook received");
