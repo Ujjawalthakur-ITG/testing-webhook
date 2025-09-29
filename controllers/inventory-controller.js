f6c179ec1fa968dee92a7f20d516edce505ccd0e
ujjwal <ujjwalthakur@itgeeks.com>
2025-09-29 17:55:48 +0530

    add file

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 const axios = require("axios");
 
+// Express handler for webhook POST request
 const webhookCreate = async (req, res) => {
-    console.log(req.body,"my paylorddd")
-  res.status(200).send("Webhook received");
+  try {
+    console.log(req.body, "my payload");
+    // You can do further processing here, e.g., call axios, etc.
+
+    res.status(200).send("Webhook received");
+  } catch (error) {
+    console.error("Error handling webhook:", error);
+    res.status(500).send("Internal Server Error");
+  }
 };
 
 module.exports = { webhookCreate };
